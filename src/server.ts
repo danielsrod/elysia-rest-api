3f638b451dd8d862a782ab34deb34d1bdb345f63
Daniel Rodrigues <coeng.daniel@gmail.com>
2023-12-11 18:23:12 -0400

    binder finalizado

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,14 @@
 import { Elysia } from "elysia";
-import { swagger } from "@elysiajs/swagger";
+import { cors } from "@elysiajs/cors";
 import { router } from "./routes/router";
+import { swagger } from "@elysiajs/swagger";
 
 export const app = new Elysia();
 
 app.use(swagger());
-
+app.use(cors());
 app.use(router);
 
 app.listen(3000);
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+console.info(`🦊 Running at ${app.server?.hostname}:${app.server?.port}\n`);
